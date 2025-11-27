d756fbd390dfd45f3fcc05bc3505a110bb3e2c6a
james7malcolm-code <james7malcolm@gmail.com>
2025-11-27 14:37:33 -0500

    Add Page component to fetch and display data

---
+++ Collect.js
@@ -0,0 +1,11 @@
+export default async function Page() {
+  const res = await fetch("https://snapchat.com", {
+    cache: "no-store"
+  });
+
+  const data = await res.json();
+
+  return (
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  );
+}
