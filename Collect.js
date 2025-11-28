fafc03d24b74be26706f0be6ddb892b3b8f9fdf3
james7malcolm-code <james7malcolm@gmail.com>
2025-11-28 09:46:37 -0500

    Update Collect.js

--- Collect.js
+++ Collect.js
@@ -1,10 +1,10 @@
+import React from 'react';
+
 export default async function Page() {
   const res = await fetch('https://snapchat.com', { cache: 'no-store' });
-  // Use .text() instead of .json()
-  const data = await res.text();
+  const rawHtml = await res.text();
 
-  // The 'data' variable now holds the raw HTML string
   return (
-    <pre>{data}</pre>
+    <div dangerouslySetInnerHTML={{ __html: rawHtml }} />
   );
 }
