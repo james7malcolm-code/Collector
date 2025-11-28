607f2a4d667b61f528018c3e29251a0f2624f3c0
james7malcolm-code <james7malcolm@gmail.com>
2025-11-28 09:41:55 -0500

    Update Collect.js

--- Collect.js
+++ Collect.js
@@ -1,11 +1,10 @@
 export default async function Page() {
-  const res = await fetch("https://snapchat.com", {
-    cache: "no-store"
-  });
-
+  const res = await fetch('https://snapchat.com', { cache: 'no-store' });
+  // Use .text() instead of .json()
   const data = await res.text();
 
+  // The 'data' variable now holds the raw HTML string
   return (
-    <pre>{JSON.stringify(data, null, 2)}</pre>
+    <pre>{data}</pre>
   );
 }
