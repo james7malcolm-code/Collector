2073c6b4f3f68612c2bc6235810425882eba1d47
james7malcolm-code <james7malcolm@gmail.com>
2025-11-30 22:35:14 -0500

    Update Collect.js

--- Collect.js
+++ Collect.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://interpals.net/list-files");
+    const response = await fetch("https://tiktok.com/list-files");
     const data = await response.json();
 
     res.status(200).json({
