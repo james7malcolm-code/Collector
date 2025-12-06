f3313c6b6d646f7b7b33b4e085f42635ce04c4b3
james7malcolm-code <james7malcolm@gmail.com>
2025-12-05 23:28:05 -0500

    Update Collect.js

--- Collect.js
+++ Collect.js
@@ -1,14 +1,3 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://tiktok.com/list-files");
-    const data = await response.json();
-
-    res.status(200).json({
-      success: true,
-      files: data
-    });
-
-  } catch (err) {
-    res.status(500).json({ error: "Failed to fetch file list" });
-  }
-}
+Priority: 10
+Host: @
+Value: mx1.improvmx.com
